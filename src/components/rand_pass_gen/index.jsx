e13f747c700a4f67b85fe47385660155873ea243
Nipun Haldar <nipun.haldar@samarth.ac.in>
2023-08-07 22:47:49 +0530

    Revert "ui update"
    This reverts commit bc04f6942aeefc82e8bd482f8ecafc9a47147de9.

--- src/components/rand_pass_gen/index.jsx
+++ src/components/rand_pass_gen/index.jsx
@@ -11,7 +11,6 @@ const Welcome = (props) => {
   const [numbers, setNumbers] = useState(true);
   const [symbols, setSymbols] = useState(false);
   const [length, setLength] = useState(8);
-  const [refreshPass, setRefreshPass] = useState(true);
 
   const generatePassword = () => {
     let validChars = "";
@@ -103,32 +102,11 @@ const Welcome = (props) => {
         </label>
       </div>
       <div
-        suppressHydrationWarning
         role="button"
         onClick={(e) => navigator.clipboard.writeText(e.target.innerText)}
       >
         {generatePassword()}
       </div>
-      <button
-        className="btn"
-        style={{ backgroundColor: "#A8EB12" }}
-        onClick={() => setRefreshPass(!refreshPass)}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          className="bi bi-arrow-clockwise"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fillRule="evenodd"
-            d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"
-          />
-          <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />
-        </svg>
-      </button>
     </div>
   );
 };
