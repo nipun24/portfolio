4842aec22841832f1af5b0093cca1984c18b89c1
Nipun Haldar <nipun.haldar@samarth.ac.in>
2025-03-27 01:20:43 +0530

    fix code

--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,5 @@
 export function GET(request) {
   const CLIENT_ID = process.env.GITHUB_CLIENT_ID;
-  const CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
   const REDIRECT_URI = process.env.REDIRECT_URI;
   const authURL = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=repo,user`;
   redirect(authURL);
